d4d9d3f255c3b2e9da7f5efbc523aee1bd92c513
João Martins <a64537@ualg.pt>
2022-06-09 22:11:11 +0100

    kjb

--- demo-code/project/settings.py
+++ demo-code/project/settings.py
@@ -83,24 +83,24 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'django',
-#         'USER': 'Admin_spd@spd-db',
-#         'PASSWORD': 'a1E!34c45',
-#         'HOST': 'spd-db.postgres.database.azure.com',
-#         'PORT': '5432',
-#         'OPTIONS': {'sslmode': 'require'},
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': '/P-1LYq<_1:1ojC}',
+        'HOST': '34.89.9.64',
+        'PORT': '3306',
+        # 'OPTIONS': {'sslmode': 'require'},
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
